66f1f7d808805252e1ae05777cfb03404085727e
Tbread <itaebread@gmail.com>
2022-10-07 09:48:21 +0900

    Modify: 누락된 API 경로 추가

--- src/main/kotlin/com/practice/delivery/controller/StoreController.kt
+++ src/main/kotlin/com/practice/delivery/controller/StoreController.kt
@@ -3,6 +3,7 @@ package com.practice.delivery.controller
 import com.practice.delivery.dto.request.RegisterStoreRequestDto
 import com.practice.delivery.dto.response.ManageRegisterStoreResponseDto
 import com.practice.delivery.dto.response.RegisterStoreResponseDto
+import com.practice.delivery.dto.response.ViewRegisterStoreRequestListResponseDto
 
 import com.practice.delivery.service.Implement.UserDetailsImpl
 import com.practice.delivery.service.StoreService
@@ -29,9 +30,19 @@ class StoreController(private var storeService: StoreService) {
         return storeService.registerStore(userDetails, req,bindingResult)
     }
 
+    @GetMapping("/register-store-request-list")
+    fun viewRegisterStoreList(@AuthenticationPrincipal userDetails: UserDetailsImpl): ViewRegisterStoreRequestListResponseDto{
+        return storeService.viewRegisterStoreRequestList(userDetails)
+    }
+
 
     @GetMapping("/accept-register-request/{id}")
     fun acceptRegisterStore(@AuthenticationPrincipal userDetails: UserDetailsImpl,id:Long):ManageRegisterStoreResponseDto{
         return storeService.acceptRegisterStoreRequest(userDetails, id)
+    }
+
+    @GetMapping("/deny-register-request/{id}")
+    fun denyRegisterStore(@AuthenticationPrincipal userDetails: UserDetailsImpl,id:Long):ManageRegisterStoreResponseDto{
+        return storeService.denyRegisterStoreRequest(userDetails, id)
     }
 }
