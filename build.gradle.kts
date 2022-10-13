4bb8e586a1de86ddc921c168be9a0691e00380a8
Tbread <itaebread@gmail.com>
2022-10-13 15:39:06 +0900

    Build: QueryDSl 종속성 추가 및 Q 클래스 경로 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
+    kotlin("kapt") version "1.7.10" //Querydsl
 }
 
 group = "com.practice"
@@ -31,19 +32,25 @@ dependencies {
     implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.4")
     implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
     implementation("org.springframework.boot:spring-boot-starter-data-redis:2.7.4")
+    implementation("com.querydsl:querydsl-jpa:5.0.0")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
-    }
+kotlin.sourceSets.main{
+    setBuildDir("$buildDir/generated/source/kapt/main")
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-    systemProperty("jasypt.encryptor.password",System.getProperty("jasypt.encryptor.password"))
-}
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        systemProperty("jasypt.encryptor.password", System.getProperty("jasypt.encryptor.password"))
+    }
