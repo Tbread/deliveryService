db06a27ae484d139c13949edb059c32571c3f216
Tbread <itaebread@gmail.com>
2022-10-14 15:01:32 +0900

    Feat: swagger ui 에서 userDetails 가 필요한 기능이 작동할 수 있도록 인증 추가
    swagger ui 에서 authorize 로 토큰을 추가하면
    userDetails 가 필요한 api 에서도 응답을 받을 수 있음

--- src/main/kotlin/com/practice/delivery/config/OpenApiConfig.kt
+++ src/main/kotlin/com/practice/delivery/config/OpenApiConfig.kt
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -17,7 +19,9 @@ class OpenApiConfig {
         info.version = "v1.0.0"
         info.description = "Spring Boot 를 이용한 배달앱 서비스 백엔드 API"
         info.contact = Contact().name("Tbread").email("itaebread@gmail.com").url("https://github.com/tbread")
-        return OpenAPI().components(Components()).info(info)
-
+        var securityScheme = SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+            .`in`(SecurityScheme.In.HEADER).name("Authorization")
+        var securityRequirement = SecurityRequirement().addList("bearerAuth")
+        return OpenAPI().info(info).components(Components().addSecuritySchemes("bearerAuth", securityScheme)).security(listOf(securityRequirement))
     }
 }
