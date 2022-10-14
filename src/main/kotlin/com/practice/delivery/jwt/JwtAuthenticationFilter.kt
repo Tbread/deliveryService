35c6ac49c1b7170bae5c545ff73fa94da27347c8
Tbread <itaebread@gmail.com>
2022-10-14 15:00:01 +0900

    Fix: Authorization 헤더에서 토큰의 앞에 'bearer ' 접두사가 붙을 수 있도록 수정

--- src/main/kotlin/com/practice/delivery/jwt/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/practice/delivery/jwt/JwtAuthenticationFilter.kt
@@ -12,10 +12,15 @@ import javax.servlet.http.HttpServletRequest
 class JwtAuthenticationFilter(private var jwtTokenProvider: JwtTokenProvider) : GenericFilterBean() {
 
     override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
-        val token: String? = jwtTokenProvider.resolveToken(request as HttpServletRequest)
-        if (Objects.nonNull(token) && jwtTokenProvider.validToken(token!!)) {
-            val authentication: Authentication = jwtTokenProvider.getAuthentication(token)
-            SecurityContextHolder.getContext().authentication = authentication
+        var token: String? = jwtTokenProvider.resolveToken(request as HttpServletRequest)
+        if (Objects.nonNull(token)) {
+            if ("bearer " in token!! || "Bearer " in token || "BEARER " in token) {
+                token = token.substring(7)
+            }
+            if (jwtTokenProvider.validToken(token)) {
+                val authentication: Authentication = jwtTokenProvider.getAuthentication(token)
+                SecurityContextHolder.getContext().authentication = authentication
+            }
         }
         chain.doFilter(request, response)
     }
