5cae6a3fc3cffa9867c00cd3491667306f1d7820
Tbread <itaebread@gmail.com>
2022-10-14 14:24:34 +0900

    Fix: WebMvcConfig 의 상속관계 변경

--- src/main/kotlin/com/practice/delivery/config/WebMvcConfig.kt
+++ src/main/kotlin/com/practice/delivery/config/WebMvcConfig.kt
@@ -4,10 +4,11 @@ import com.practice.delivery.utils.CustomInterceptor
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 
 @Configuration
-class WebMvcConfig: WebMvcConfigurationSupport() {
+class WebMvcConfig: WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(CustomInterceptor())
