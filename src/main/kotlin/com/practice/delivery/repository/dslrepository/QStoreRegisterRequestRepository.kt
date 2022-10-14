9b988417cb2c2f0fec53880e8ef550db81199322
Tbread <itaebread@gmail.com>
2022-10-14 19:30:59 +0900

    Feat: StoreRegisterRequest 쿼리 DSL 레포지토리 추가

---
+++ src/main/kotlin/com/practice/delivery/repository/dslrepository/QStoreRegisterRequestRepository.kt
@@ -0,0 +1,21 @@
+package com.practice.delivery.repository.dslrepository
+
+import com.practice.delivery.entity.QStoreRegisterRequest
+import com.practice.delivery.entity.StoreRegisterRequest
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.stereotype.Repository
+
+@Suppress("SpringJavaInjectionPointsAutowiringInspection")
+@Repository
+class QStoreRegisterRequestRepository(private var jpaQueryFactory: JPAQueryFactory) {
+
+    fun findByStatus(status: StoreRegisterRequest.Status): List<StoreRegisterRequest> {
+        return jpaQueryFactory.selectFrom(QStoreRegisterRequest.storeRegisterRequest)
+            .where(QStoreRegisterRequest.storeRegisterRequest.status.eq(status))
+            .join(QStoreRegisterRequest.storeRegisterRequest.owner)
+            .join(QStoreRegisterRequest.storeRegisterRequest.approver)
+            .fetchJoin()
+            .fetch()
+    }
+
+}
