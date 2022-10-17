410f0993a795125c2f933ebbe385e0e09e1ab876
Tbread <itaebread@gmail.com>
2022-10-17 16:56:59 +0900

    Feat: 배너 컨트롤러 OpenApi 설명 추가

--- src/main/kotlin/com/practice/delivery/controller/BannerController.kt
+++ src/main/kotlin/com/practice/delivery/controller/BannerController.kt
@@ -6,15 +6,22 @@ import com.practice.delivery.dto.response.DefaultResponseDto
 import com.practice.delivery.dto.response.ViewBannerListResponseDto
 import com.practice.delivery.service.BannerService
 import com.practice.delivery.service.implemented.UserDetailsImpl
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
+@Tag(name = "배너 관련")
 @RestController
 @RequestMapping("/banner")
 class BannerController(private var bannerService: BannerService) {
 
+    @Operation(
+        summary = "배너 등록 API",
+        description = "새로운 배너를 정의하여 등록합니다.<br />헤더에 Authorization 으로 JWT 토큰을 요구합니다."
+    )
     @PostMapping("/add")
     fun addBanner(
         @AuthenticationPrincipal userDetails: UserDetailsImpl,
@@ -24,13 +31,23 @@ class BannerController(private var bannerService: BannerService) {
         return bannerService.addBanner(userDetails, req, bindingResult)
     }
 
+    @Operation(
+        summary = "배너 만료 API",
+        description = "배너를 만료처리합니다. 해당 배너는 만료일자까지 배너 조회 API 를 통해 조회할 수 있습니다.<br />헤더에 Authorization 으로 JWT 토큰을 요구합니다."
+    )
     @DeleteMapping("/expire")
-    fun expireBanner(@AuthenticationPrincipal userDetails: UserDetailsImpl,@PathVariable id:Long):DefaultResponseDto{
+    fun expireBanner(
+        @AuthenticationPrincipal userDetails: UserDetailsImpl,
+        @PathVariable id: Long
+    ): DefaultResponseDto {
         return bannerService.expireBanner(userDetails, id)
     }
 
+    @Operation(
+        summary = "배너 조회 API"
+    )
     @GetMapping("/view")
-    fun viewBannerList():ViewBannerListResponseDto{
+    fun viewBannerList(): ViewBannerListResponseDto {
         return bannerService.viewBannerList()
     }
 }
