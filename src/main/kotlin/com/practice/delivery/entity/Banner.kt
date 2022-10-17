f91716efb235439aa25feee244472c526641a994
Tbread <itaebread@gmail.com>
2022-10-17 15:07:53 +0900

    Feat: 배너 엔티티 추가

--- src/main/kotlin/com/practice/delivery/entity/Banner.kt
+++ src/main/kotlin/com/practice/delivery/entity/Banner.kt
@@ -1,4 +1,28 @@
 package com.practice.delivery.entity
 
+import javax.persistence.*
+
+@Entity
 class Banner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0
+
+    @Column(nullable = false)
+    var imgSrc:String
+
+    @Column(nullable = false)
+    var src:String
+
+    @ManyToOne
+    @JoinColumn
+    var user:User
+
+
+    constructor(imgSrc:String,src:String,user: User){
+        this.imgSrc = imgSrc
+        this.src = src
+        this.user = user
+    }
 }
